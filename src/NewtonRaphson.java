d8806580a9538d15ae024a2780758975cfdb76b1
Isaiah Jordan <idjordan@cpp.edu>
2023-11-19 11:22:00 -0800

    Secant and Newton methods

--- src/NewtonRaphson.java
+++ src/NewtonRaphson.java
@@ -1,5 +1,47 @@
 public class NewtonRaphson extends RootMethod {
-    //xi+1= xi - f(xi)/f'(xi)
+    private double xNext=0;
     
+    //xi+1= xi - f(xi)/f'(xi)
+    @Override
+    void findRoot1() {
+        //xi=boundB
+        double functionX= processFunction1(boundB);
+        double derivativeX= processDerivative1(boundB);
+
+        xNext = boundB-(functionX/derivativeX);
+        
+    }
+
+    @Override
+    void findRoot2() {
+        //xi=boundB
+        double functionX= processFunction2(boundB);
+        double derivativeX= processDerivative2(boundB);
+
+        xNext = boundB-(functionX/derivativeX);
+    }
    
+    double processDerivative1(double x){
+        System.out.println("6x^2-23.4x+17.7");
+        double tempDouble=0;
+
+        tempDouble = (6*Math.pow(x, 2)-(23.4*x)+17.7);
+        return tempDouble;
+    }
+
+    double processDerivative2(double x){
+        System.out.println("[(50sinh(50/x))/x]-cosh(50/x)+1");
+        double tempDouble=0;
+
+        tempDouble=(((50*Math.sinh(50/x))/x) - Math.cosh(50/x)+1);
+        return tempDouble;
+    }
+
+    public double getxNext() {
+        return xNext;
+    }
+
+    public void setxNext(double xNext) {
+        this.xNext = xNext;
+    }
 }
--- src/Secant.java
+++ src/Secant.java
@@ -1,5 +1,31 @@
 public class Secant extends RootMethod {
+    double xNext=0;
     //xi+1= xi -f(xi)/[(f(xi)-f(xi-1)/(xi-(xi-1)))]
+    @Override
+    void findRoot1() {
+        //xi=boundB
+        //xi-1=boundA
+        double functionX = processFunction1(boundB);
+        double functionXPrev= processFunction1(boundA);
+
+        xNext=(boundB-functionX)/((functionX-functionXPrev)/(boundB-boundA));
+        
+    }
+
+    @Override
+    void findRoot2() {
+        //xi=boundB
+        //xi-1=boundA
+        double functionX = processFunction2(boundB);
+        double functionXPrev= processFunction2(boundA);
+
+        xNext=(boundB-functionX)/((functionX-functionXPrev)/(boundB-boundA));
+    }
     
-    
+    public double getxNext() {
+        return xNext;
+    }
+    public void setxNext(double xNext) {
+        this.xNext = xNext;
+    }
 }
